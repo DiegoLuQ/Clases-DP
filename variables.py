45443f42de43b476a7a51b926189ddb970af4060
Diego <juan.luque@colegiomacaya.cl>
2023-10-18 13:05:28 -0300

    first commit

---
+++ variables.py
@@ -0,0 +1,23 @@
+# String
+nombre = "Diego"
+# Integer
+edad = 28
+# Bool
+casado = False
+# Lista
+idiomas = ["Español", "Ingles", "Chileno"]
+
+lista_de_cosas = ["Tv", 9000, True, 72.2, {'nombre': 'Diego'}]
+
+# Float
+peso_kg = 72.2
+
+
+informacion_profesor = {
+    'nombre': 'Diego',
+    'edad': 28,
+    'casado': False,
+    'idiomas': ["Español", "Ingles", "Chileno"],
+    'peso_kg': 72.2,
+    "padres": [{"nombre": "Santiago"}, {"nombre": "Ruth"}]
+}
